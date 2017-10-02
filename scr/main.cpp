285e6a49c78fb697dd6933065a1a62d6e319b07c
Marty Balz Jakob Gabriel <bmarty@SVBBCF20170029.intranet.epfl.ch>
2017-10-02 14:11:51 +0200

    class Neuron

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,6 +1,14 @@
 #include "neuron.hpp"
+#include <iostream>
 
 using namespace std;
 
 int main()
-{}
+
+{
+	const double I_ext(0);
+	
+	cout << "Hello"<< endl;
+	//const 
+	
+	}
--- scr/neuron.hpp
+++ scr/neuron.hpp
@@ -1,1 +1,18 @@
+#ifndef NEURON_H
+#define NEURON_H
 
+#include <iostream>
+
+class Neuron
+{
+	public:
+	
+	void update();
+	
+	private:
+	
+	//double 
+};
+
+
+#endif
